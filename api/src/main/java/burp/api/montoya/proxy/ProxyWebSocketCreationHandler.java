4d5ba0d4acadc9b2bdd3d0565caad7044469eca6
Sean Burns <sean.burns@portswigger.net>
2022-12-29 09:36:55 +0000

    BURP-9722. Wrap parameters of ProxyWebSocketCreationHandler.

--- api/src/main/java/burp/api/montoya/proxy/ProxyWebSocketCreationHandler.java
+++ api/src/main/java/burp/api/montoya/proxy/ProxyWebSocketCreationHandler.java
@@ -8,8 +8,6 @@
 
 package burp.api.montoya.proxy;
 
-import burp.api.montoya.http.message.requests.HttpRequest;
-
 /**
  * Extensions can implement this interface and then call {@link Proxy#registerWebSocketCreationHandler} to register a WebSocket handler.
  * The handler will be notified of new WebSockets created by the Proxy tool.
@@ -18,10 +16,10 @@ public interface ProxyWebSocketCreationHandler
 {
     /**
      * This method is invoked by Burp when a WebSocket is being created by the Proxy tool.
-     * Note that the client side of the connection will not be upgraded until after this method completes.
+     * <br>
+     * <b>Note</b> that the client side of the connection will not be upgraded until after this method completes.
      *
-     * @param webSocket      The ProxyWebSocket that is being created.
-     * @param upgradeRequest The HTTP upgrade request that initiated the WebSocket creation.
+     * @param webSocketCreation {@link ProxyWebSocketCreation} containing information about the proxy websocket that is being created
      */
-    void handleWebSocketCreation(ProxyWebSocket webSocket, HttpRequest upgradeRequest);
+    void handleWebSocketCreation(ProxyWebSocketCreation webSocketCreation);
 }

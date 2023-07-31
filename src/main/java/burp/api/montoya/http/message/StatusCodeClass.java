4387ddabae496aedd15781bc6c17203648789517
James Armitstead <james.armitstead@portswigger.net>
2023-07-31 11:26:13 +0100

    BURP-10157. Stop StatusCodeClass having a dependency on Range since there is no guarantee that Range will be available because the MontoyaObjectFactory may be null.

--- src/main/java/burp/api/montoya/http/message/StatusCodeClass.java
+++ src/main/java/burp/api/montoya/http/message/StatusCodeClass.java
@@ -1,32 +1,28 @@
 package burp.api.montoya.http.message;
 
-import burp.api.montoya.core.Range;
-
 /**
  * Status code classes that are defined in the HTTP standard.
  */
 public enum StatusCodeClass
 {
-    CLASS_1XX_INFORMATIONAL_RESPONSE(Range.range(100, 200)),
-    CLASS_2XX_SUCCESS(Range.range(200, 300)),
-    CLASS_3XX_REDIRECTION(Range.range(300, 400)),
-    CLASS_4XX_CLIENT_ERRORS(Range.range(400, 500)),
-    CLASS_5XX_SERVER_ERRORS(Range.range(500, 600));
-
-    private final Range range;
-
-    StatusCodeClass(Range range)
-    {
-        this.range = range;
-    }
-
     /**
-     * @param statusCode The status code to test.
-     *
-     * @return True if the status code is in the status code class.
+     * The Enum that represents status codes 100 to 199.
      */
-    public boolean contains(short statusCode)
-    {
-        return range.contains(statusCode);
-    }
+    CLASS_1XX_INFORMATIONAL_RESPONSE,
+    /**
+     * The Enum that represents status codes 200 to 299.
+     */
+    CLASS_2XX_SUCCESS,
+    /**
+     * The Enum that represents status codes 300 to 399.
+     */
+    CLASS_3XX_REDIRECTION,
+    /**
+     * The Enum that represents status codes 400 to 499.
+     */
+    CLASS_4XX_CLIENT_ERRORS,
+    /**
+     * The Enum that represents status codes 500 to 599.
+     */
+    CLASS_5XX_SERVER_ERRORS
 }
